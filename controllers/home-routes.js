ade434a88132755f73d3faa1e76e6b058e51ec11
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-02-22 07:45:48 -0800

    added get post by id on home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,3 +34,49 @@ router.get('/',(req,res) => {
     res.status(500).json(err);
 });
 });
+
+// Get one post by ID
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'There are no post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({  plain: true });
+            // Give data to template
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Get login information
