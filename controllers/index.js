5245e3db4fd5ff1f59ca90deaff8c1c53cc52dce
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-02-22 11:17:38 -0800

    trial

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,10 @@ const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 
-//outer.use('/api', apiRoutes);
+//r outer.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+
 
 router.use((req, res) => {
     res.status(404).end();
