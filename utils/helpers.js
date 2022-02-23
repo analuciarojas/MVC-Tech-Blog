005254ab75ce129e3fe9171e77c0eca361579d49
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-02-23 13:41:21 -0800

    added date helper and aunthenticator user functions

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+// Get date 
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+}
+
+
+module.exports = {
+    formatDate
+}
