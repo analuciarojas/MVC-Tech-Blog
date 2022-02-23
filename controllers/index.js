c133e660a6153348986716ee1cbfb1abe00eeb19
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-02-22 20:13:08 -0800

    added basic html structures and comment model, fixed bugs on home routes

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,9 @@ const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 
-//r outer.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+//router.use('/api', apiRoutes);
 
 
 router.use((req, res) => {
