147b2365ad108aea1c4feb22e9234775b729b74a
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-02-23 13:07:22 -0800

    added login form handler

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,27 @@
-// Login form handler
+// Login form handler 
 async function loginFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
+    // Check not empty 
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/user/login', {
             method: 'post',
             body: JSON.stringify({
                 username,
                 password
             }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            headers: {'Content-Type': 'application/json'}
         });
-
         if (response.ok) {
             document.location.replace('/dashboard');
         } else {
-            alert(response.statusText);
+            let result = await response.json()
+            alert(result.message)
         }
     }
 }
 
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
